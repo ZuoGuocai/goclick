e7897987ee003f7f958f5b41acf58015362a457a
Zuo Guocai <zuoguocai@126.com>
2017-04-26 21:11:16 +0800

    One click step to Baidu Search Engine

---
+++ gobaidu.go
@@ -0,0 +1,24 @@
+/* ************************************* *\
+|     One Click to Baidu  Search Engine   |
+|                                         |
+|     Author: zuoguocai@126.com           |
+|                                         |
+|     Last_change: 2017年4月26日          |
+|                                         |
+|     Apache Licene 2.0                   |
+\* ************************************* */
+
+
+
+package main
+
+import (
+"github.com/toqueteos/webbrowser"
+)
+
+func main() {
+
+
+	webbrowser.Open("https://www.baidu.com/")
+
+}
